b9696c9b131dd4151668fc1226f23cf0cd1aae8c
s.a. faroque <faroque.computer@gmail.com>
2023-09-08 12:36:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function loadFn(){
+  
+  ourRequest = new XMLHttpRequest();
+  // console.log(ourRequest);
+
+
+
+  ourRequest.open("GET", "faroque.JSON");
+  ourRequest.onload = function(){
+
+    
+  }
+  ourRequest.send();
+
+  
+  // clickMe.innerHTML = 'hi';
+}
